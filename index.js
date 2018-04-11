05c828adc1bb1d138ad7e97846dface4b43965d8
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-11 21:07:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
+function happyHolidaysTo(holiday, name) {
+  return "Happy Independence Day, you!"
+  
+}
